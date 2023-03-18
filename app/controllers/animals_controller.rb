1bdb49947e731b408aa69cfdd9b745b47a3d1ba3
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-03-18 15:00:00 +0200

    added comments

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,50 +1,66 @@
 class AnimalsController < ApplicationController
   include ActionController::Cookies
-  
+
+  # Get all animals and their comments, including replies
   def index
     animals = Animal.all.includes(:comments)
     render json: animals.as_json(include: { comments: { include: :replies } })
   end
-    
+
+  # Get an animal by ID and its comments, including replies
   def show
     animal = Animal.includes(comments: [:replies]).find_by(id: params[:id])
     render json: animal
   end
 
+  # Create a new animal
   def create
-  animal = Animal.create(animal_params)
-  render json: animal, status: :created
+    animal = Animal.create(animal_params)
+    render json: animal, status: :created
   end
-  
+
+  # Update the likes for an animal
   def update
     animal = Animal.find(params[:id])
     user_id = params[:user_id]
     like = animal.likes.find_by(user_id: user_id)
-  
+
+    # If the user has already liked the animal, remove the like
     if like
       like.destroy
+    # If the user has not liked the animal, add a new like
     else
       like = animal.likes.create(user_id: user_id)
     end
-  
-    animal.reload # To get the updated count of likes
+
+    # Reload the animal to get the updated count of likes and render the updated animal
+    animal.reload
     render json: animal, include: :user
   end
 
+  # Update the likes count for an animal
   def update_likes
     animal = Animal.find(params[:animal_id])
     user_id = params[:user_id]
     like = animal.likes.find_by(user_id: user_id)
-  
+
+    # If the user has already liked the animal, remove the like
     if like
       like.destroy
+    # If the user has not liked the animal, add a new like
     else
       like = animal.likes.create(user_id: user_id)
     end
-  
+
+    # Render the updated likes count for the animal
     render json: { likes_count: animal.likes.count }
   end
-  
+
+  private def animal_params
+    params.require(:animal).permit(:name, :species, :description, :image_url)
+  end
+end
+
   ####################
   #      COMMENTS    #
   ####################
@@ -54,8 +70,6 @@ class AnimalsController < ApplicationController
     @comment.save
     render json: @comment
   end
-  
-
 
   def update_comment_likes
     comment = Comment.find(params[:comment_id])
@@ -86,8 +100,6 @@ class AnimalsController < ApplicationController
     reply = comment.replies.create(reply_params)
     render json: reply, status: :created
   end
-  
-    
   
   private
 
