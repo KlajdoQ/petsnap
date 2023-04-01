d8966d181dc5791a1cf5fe11b54e6f65b647c4be
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-04-01 16:00:00 +0223

    added comments

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,7 +2,6 @@ class Animal < ApplicationRecord
     belongs_to :user 
     has_many :likes
     has_many :comments, dependent: :destroy
-
     has_many :replies, through: :comments
     accepts_nested_attributes_for :comments
-  end
+end
