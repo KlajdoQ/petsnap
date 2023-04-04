7262cdcd176f55f1eda2ab002aa165445e9225cc
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-04-04 13:11:58 -0400

    render

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :recipes, only: [:index, :create]
+    post "/signup", to: "users#create"
+    get "/me", to: "users#show"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+  end
 
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   
   resources :users
   post '/login', to: 'sessions#create'
