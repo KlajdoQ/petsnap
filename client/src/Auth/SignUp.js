8f6b732a7dd421da6ce7dbcfd22624bd0ed48f2e
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-04-10 16:00:00 +0223

    added comments

--- client/src/Auth/SignUp.js
+++ client/src/Auth/SignUp.js
@@ -70,69 +70,68 @@ export default function SignUp({setUser}) {
   };
   return (
     <div className="signup-page">
-
-    <div className="Auth-form-container">
-      <form className="Auth-form" onSubmit={handleSignUpSubmit}>
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign Up</h3>
-          <div className="text-center">
-            Already registered?{" "}
-            <span className="link-primary" onClick={changeAuthMode}>
-              Log In
-            </span>
-          </div>
-          <div className="form-group mt-3">
-            <label>Full Name</label>
-            <input
-              type="text"
-              className="form-control mt-1"
-              placeholder="e.g Jane Doe"
-              value={signUpForm.full_name}
-              name="full_name"
-            onChange={handleSignUpFormChange}
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Email Address"
-              value={signUpForm.email}
+      <div className="Auth-form-container">
+        <form className="Auth-form" onSubmit={handleSignUpSubmit}>
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign Up</h3>
+            <div className="text-center">
+              Already registered?{" "}
+              <span className="link-primary" onClick={changeAuthMode}>
+                Log In
+              </span>
+            </div>
+            <div className="form-group mt-3">
+              <label>Full Name</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+                placeholder="e.g Jane Doe"
+                value={signUpForm.full_name}
+                name="full_name"
               onChange={handleSignUpFormChange}
-              name="email"
-            />
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Email Address"
+                value={signUpForm.email}
+                onChange={handleSignUpFormChange}
+                name="email"
+              />
+            </div>
+            <div className="form-group">
+            <label htmlFor="image">Image:</label>
+            <input 
+              type="file" 
+              name="image" 
+              id="image" 
+              onChange={handleImageChange}
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Password"
+                value={signUpForm.password}
+                onChange={handleSignUpFormChange}
+                name="password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+            <p className="text-center mt-2">
+            </p>
           </div>
-          <div className="form-group">
-          <label htmlFor="image">Image:</label>
-          <input 
-            type="file" 
-            name="image" 
-            id="image" 
-            onChange={handleImageChange}
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Password"
-              value={signUpForm.password}
-              onChange={handleSignUpFormChange}
-              name="password"
-            />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-          <p className="text-center mt-2">
-          </p>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
     </div>
   )
 }
