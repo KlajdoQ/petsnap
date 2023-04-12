1fbf182570faca29b8950f68300eb585f073591c
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-04-12 16:00:00 +0223

    added comments

--- client/src/components/cats/CatFact.js
+++ client/src/components/cats/CatFact.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 
-// Create a styled component for the CatFactDiv
-const CatFactDiv = styled.li`
-  margin-left: 20px;
-  margin-bottom: 30px;
-`;
 
-// Create a functional component for the CatFact
 export default function CatFact({ fact }) {
   return (
-        // Use the styled component to render the fact
     <CatFactDiv>
         {fact.fact}
     </CatFactDiv>
   )
 }
+
+
+const CatFactDiv = styled.li`
+  margin-left: 20px;
+  margin-bottom: 30px;
+`;
