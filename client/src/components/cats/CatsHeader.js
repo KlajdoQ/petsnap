261026250d582a5bedf9fa5647739711d4d9a26c
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-04-14 16:00:00 +0223

    added comments

--- client/src/components/cats/CatsHeader.js
+++ client/src/components/cats/CatsHeader.js
@@ -2,21 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 
 
-const Header = styled.div 
-` background-color: rgb(237, 126, 126);
-color:white;
-width:190px;
-padding:10px;
-font-weight: bold;
-font-size: 20px;
-border-radius: 5px;
-text-align: center;
-margin-left:10px;
-margin-bottom:20px;`
-
-
 export default function CatsHeader() {
   return (
     <Header>Todays Cat Facts</Header>
-  )
-}
+    )
+  }
+  
+  
+  const Header = styled.div 
+  ` background-color: rgb(237, 126, 126);
+  color:white;
+  width:190px;
+  padding:10px;
+  font-weight: bold;
+  font-size: 20px;
+  border-radius: 5px;
+  text-align: center;
+  margin-left:10px;
+  margin-bottom:20px;`
+  
