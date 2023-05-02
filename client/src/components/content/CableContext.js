5b63651233059e8379a1c1d016fb515b1bf93851
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-05-02 16:00:00 +0223

    added comments

--- client/src/components/content/CableContext.js
+++ client/src/components/content/CableContext.js
@@ -1,22 +1,29 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { createConsumer } from '@rails/actioncable';
 
+// Create a new context for Action Cable
 const CableContext = createContext();
 
+// Create a provider component for the CableContext
 const CableProvider = ({ children }) => {
   const [cable, setCable] = useState(null);
   const [receivedMessage, setReceivedMessage] = useState(null);
+
   const handleReceivedMessage = (message) => {
     setReceivedMessage(message);
   };
   
+  // Create a new consumer when the component mounts
   useEffect(() => {
     const cableConsumer = createConsumer('ws://localhost:3000/cable');
+    // Update the state with the new consumer
     setCable(cableConsumer);
 
+    // Return a function to disconnect the consumer when the component unmounts
     return () => cableConsumer.disconnect();
   }, []);
 
+  // Render the CableContext.Provider and pass in the state and handleReceivedMessage function as the value
   return (
     <CableContext.Provider value={{ cable, receivedMessage, handleReceivedMessage }}>
       {children}
@@ -24,5 +31,6 @@ const CableProvider = ({ children }) => {
   )
 }
 
+// Export the CableContext and CableProvider for use in other components
 export { CableContext, CableProvider };
 
